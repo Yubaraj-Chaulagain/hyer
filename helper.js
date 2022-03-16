5df9e33fabf82e2305333be22ede1c69155efe1a
Yubaraj-Chaulagain <100876345+Yubaraj-Chaulagain@users.noreply.github.com>
2022-03-16 06:15:06 +0545

    new

--- helper.js
+++ helper.js
@@ -8,7 +8,7 @@ document.querySelector('#find').addEventListener('click',function(e){
     var tenure=document.getElementById('tenure').value;
    
     var noOfMonths=Math.floor(tenure)*12;
-    var monthlyRate=interestRate/(12*180);
+    var monthlyRate=interestRate/(12*100);
     var onePlusR=Math.pow(1+monthlyRate,noOfMonths)
     var denominator=onePlusR-1;
     var emi=(loan*monthlyRate*(onePlusR/denominator)).toPrecision(5);
