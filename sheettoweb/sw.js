91f8b254856834f671d449f28cc2ec4093074831
Yubaraj Chaulagain <100876345+Yubaraj-Chaulagain@users.noreply.github.com>
2026-03-11 13:44:47 +0400

    Create sw.js

---
+++ sheettoweb/sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'chat-v1';
+const assets = ['./', './index.html', './chatnp.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
